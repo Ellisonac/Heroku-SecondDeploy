d5c120612c212c59ea4470ce5735c8ecd2dde107
Andrew Ellison <acannonellison@gmail.com>
2021-11-22 21:03:03 -0800

    Port modification

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const express = require('express');
 const path = require('path');
 const { clog } = require('./middleware/clog');
 const api = require('./routes/index.js');
-const diagnostics = require('./routes/diagnostics.js');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
